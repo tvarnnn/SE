3707e74d391653e433a6c7b04df0a9caa914192e
tristan <tv.lloyd.varner@gmail.com>
2025-09-17 18:29:58 -0400

    Update RayBank.py
    Created protected and private members for account number and routing number

--- RayBank.py
+++ RayBank.py
@@ -1,14 +1,19 @@
 class RayBank:
     bank_name = "RayBank"
 
-    def __init__(self,customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
 
-    
-    def deposit(self,deposit):
-        if isinstance(deposit, (int,float)):
+        # Protected member | Tristan
+        self._account_number = account_number  
+
+        # Private member | Tristan
+        self.__routing_number = routing_number  
+
+    def deposit(self, deposit):
+        if isinstance(deposit, (int, float)):
             self.current_balance += deposit
             print("Your current balance is: ", self.current_balance)
         else:
@@ -16,31 +21,25 @@ class RayBank:
     
     def withdraw(self, deposit):
         if self.current_balance - deposit < self.minimum_balance:
-            print("You have a balance of",self.current_balance,". This is an insufficient amount of funds. Please deposit more") 
+            print("You have a balance of", self.current_balance, 
+                  ". This is an insufficient amount of funds. Please deposit more") 
         else:
             self.current_balance -= deposit
-            print("Withdrawal succesful, Current balance so far: ", self.current_balance)
+            print("Withdrawal successful, Current balance so far: ", self.current_balance)
     
-
     def print_customer_information(self):
         print(f"Welcome to: {RayBank.bank_name}")
         print("Welcome", self.customer_name)
-
         print("Current Balance:", self.current_balance)
+        print("Account Number (protected):", self._account_number)
+        print("Routing Number (private):", self.__routing_number)
         print("-----------------------------------")
-
         print("As a reminder the minimum balance for withdrawals is: ", self.minimum_balance)
-        
-    class SavingsAcc(Person):
-        def interest(RayBank):
-            if self.current_balance
 
 
-        
-
-
-customer1 = RayBank("Jane", 500, 100)
-customer2 = RayBank("Maxwell", 1000, 200)
+# Example usage
+customer1 = RayBank("Jane", 500, 100, "12345678", "111000025")
+customer2 = RayBank("Maxwell", 1000, 200, "87654321", "222000111")
 
 customer1.deposit(200)
 customer1.withdraw(700)  
@@ -48,4 +47,3 @@ customer1.print_customer_information()
 
 customer2.withdraw(850) 
 customer2.print_customer_information()
-            

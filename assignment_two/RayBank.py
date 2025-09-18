601a7fc5e8d8573b5a9033d669c08c8a95a3e726
Raymundo <raymundomc0205@icloud.com>
2025-09-17 20:35:53 -0400

    savingsAcc

--- assignment_two/RayBank.py
+++ assignment_two/RayBank.py
@@ -37,7 +37,12 @@ class RayBank:
 
     print("TEST TO SEE IF PULL REQUEST WILL WORK")
 
-
+class SavingsAcc(RayBank):
+    def interest(self):
+        if self.current_balance > 200:
+            print("You are due to gain some interest on your crash if you keep", self.current_balance,"in the bank over time we will give you interest at a small rate into your account")
+        else:
+            print("Not enough money, need at least 200 to maintain balance without harm of fees or limitation of services.")
         
 
 

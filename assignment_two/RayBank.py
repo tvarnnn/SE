d1a9008c2017be90db40204783c90cd22e79e7fa
Raymundo <raymundomc0205@icloud.com>
2025-09-17 20:50:23 -0400

    merge

--- assignment_two/RayBank.py
+++ assignment_two/RayBank.py
@@ -45,6 +45,16 @@ class SavingsAcc(RayBank):
             print("Not enough money, need at least 200 to maintain balance without harm of fees or limitation of services.")
         
 
+
+
+class SavingsAcc(RayBank):
+    def interest(self):
+        if self.current_balance > 200:
+            print("You are due to gain some interest on your crash if you keep", self.current_balance,"in the bank over time we will give you interest at a small rate into your account")
+        else:
+            print("Not enough money, need at least 200 to maintain balance without harm of fees or limitation of services.")
+
+
 class checking_limitation(RayBank):
     def checking(self):
         if 

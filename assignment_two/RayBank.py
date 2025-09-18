3a44d9a96cedad456ce6b1c031507b8afd190cae
Raymundo <raymundomc0205@icloud.com>
2025-09-17 21:02:27 -0400

    commit this

--- assignment_two/RayBank.py
+++ assignment_two/RayBank.py
@@ -31,20 +31,6 @@ class RayBank:
 
         print("As a reminder the minimum balance for withdrawals is: ", self.minimum_balance)
         
-    class SavingsAcc(Person):
-        def interest(RayBank):
-            if self.current_balance
-
-    print("TEST TO SEE IF PULL REQUEST WILL WORK")
-
-class SavingsAcc(RayBank):
-    def interest(self):
-        if self.current_balance > 200:
-            print("You are due to gain some interest on your crash if you keep", self.current_balance,"in the bank over time we will give you interest at a small rate into your account")
-        else:
-            print("Not enough money, need at least 200 to maintain balance without harm of fees or limitation of services.")
-        
-
 
 
 class SavingsAcc(RayBank):
